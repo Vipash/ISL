1a1cbdcdde82637fedc7bca448ab05829825414b
Vipash <vipashmeena@gmail.com>
2026-05-15 09:40:36 +0530

    streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="ISL Hand Sign Translator", layout="wide")
+
+st.title("ISL Hand Sign Translator")
+st.write("This is a placeholder Streamlit UI. Webcam integration will be added next.")
+
+st.markdown("""
+- Press **1** in the desktop app to speak current word.
+- Use **2/3** for sentence mode in the desktop version.
+""")
